d4db1e6384c5fb4f28fe30d239e31e7697bc33dd
euny0ng <h_lyn@hanmail.net>
2025-07-24 19:24:16 +0900

    add target chase camera

--- Assets/Scripts/Entity/FollowCamera.cs
+++ Assets/Scripts/Entity/FollowCamera.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    float offsetX;
+    float offsetY;
+
     void Start()
     {
-        
+        if (target == null)
+        {
+            Debug.Log("타겟을 설정해라");
+            return;
+        }
+
+        offsetX = transform.position.x - target.position.x;
+        offsetY = transform.position.y - target.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 pos = transform.position;
+        pos.x = target.position.x + offsetX;
+        pos.y = target.position.y + offsetY;
+        transform.position = pos;
     }
 }
